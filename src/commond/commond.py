c7b2ab2455e950df42091e4f6715cba60bf66629
yiliang <yapyiliangww321@gmail.com>
2025-02-11 22:39:51 +0800

    add password compare

--- src/commond/commond.py
+++ src/commond/commond.py
@@ -1,3 +1,4 @@
+import bcrypt
 from enum import Enum
 
 class Auth(Enum):
@@ -8,3 +9,6 @@ class Auth(Enum):
 class ResultType(Enum):
     NORMAL = 'NORMAL'
     IMAGE = 'IMAGE'
+
+def compareHash(inputPass, dbPass):
+     return bcrypt.checkpw(inputPass.encode('utf-8'), dbPass.encode('utf-8'))
