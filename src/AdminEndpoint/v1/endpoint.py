8d35647cfcc49bab87181eeb14273050cfe33b32
yiliang <yapyiliangww321@gmail.com>
2025-02-11 22:38:31 +0800

    add login endpoint

--- src/AdminEndpoint/v1/endpoint.py
+++ src/AdminEndpoint/v1/endpoint.py
@@ -1,9 +1,12 @@
 import os
 from ApiBase.ApiBase import ApiBase
 from commond.commond import Auth
-from .middleware import test
+from .middleware import test, onLogin
 
 version = os.path.basename(os.path.dirname(__file__))
 
 # GET
 ApiBase.get(f'/{version}/test', test, ['admin'], Auth.Bearer)
+
+# POST
+ApiBase.post(f'/{version}/login', onLogin, ['admin'], Auth.Bearer)
