4249c92b196a901a190f78b5b4507a65ec8a9d58
yiliang <yapyiliangww321@gmail.com>
2025-02-11 22:38:19 +0800

    add endpoint middleware

--- src/AdminEndpoint/v1/middleware.py
+++ src/AdminEndpoint/v1/middleware.py
@@ -1,2 +1,51 @@
-def test(request):
+from quart import jsonify, make_response
+from ApiBase.ApiBase import ApiBase
+from validateParam.pydantic import LoginValidate
+from MysqlBase.MysqlBase import MysqlService
+
+async def test(request):
      return 'test test'
+
+async def onLogin(request):
+     isValid, data = LoginValidate(request)
+     
+     if not isValid:
+          return jsonify({"msg": f'Invalid input - {data}'})
+     
+     res = await MysqlService.login(data)
+     
+     if res.get('validate') is True:
+          tokenInfor = {
+               'id': res.get('id'),
+               'username': data.username,
+               'role': data.role
+          }
+          print('tokenInfor: ', tokenInfor)
+
+          token = ApiBase.generateToken(tokenInfor)
+
+          response = await make_response(jsonify(
+               {
+                    'ret': 0, 
+                    'data': {
+                         'status': 0,
+                         'msg': 'Login successful'
+                    }
+               }
+          ))
+          response.set_cookie(
+               'authToken',
+               token,
+               httponly=True,  # Prevent JavaScript access
+               secure=False,  # HTTPS only
+               samesite='Strict',  # Prevent CSRF
+               max_age=3600  # 1 hour expiration
+          )
+
+          await MysqlService.updateAdminLoginTime(data.username)
+          return response
+     else:
+          return {  
+               'status': -1,
+               'msg': 'Invalid credential'
+          }
