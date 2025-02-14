966f9c99b2eb28cdbb196c97a6d74a62ba80badd
yiliang <yapyiliangww321@gmail.com>
2025-02-14 22:20:24 +0800

    add update product endpoint

--- src/AdminEndpoint/v1/endpoint.py
+++ src/AdminEndpoint/v1/endpoint.py
@@ -1,7 +1,7 @@
 import os
 from ApiBase.ApiBase import ApiBase
 from commond.commond import Auth
-from .middleware import test, onLogin, onLogout, onGetProducts
+from .middleware import test, onLogin, onLogout, onGetProducts, onUpdateProductDescribe, onUpdateProductPerantId, onUpdateProductDetail, onUpdateProductStatus, onDeleteProduct, onAddProduct
 
 version = os.path.basename(os.path.dirname(__file__))
 
@@ -12,3 +12,9 @@ ApiBase.get(f'/{version}/products', onGetProducts, ['admin'], Auth.Bearer)
 # POST
 ApiBase.post(f'/{version}/login', onLogin, ['admin'], Auth.Bearer)
 ApiBase.post(f'/{version}/logout', onLogout, ['admin'], Auth.Cookie)
+ApiBase.post(f'/{version}/update/product/describe', onUpdateProductDescribe, ['admin'], Auth.Cookie)
+ApiBase.post(f'/{version}/update/product/detail', onUpdateProductDetail, ['admin'], Auth.Cookie)
+ApiBase.post(f'/{version}/update/product/status', onUpdateProductStatus, ['admin'], Auth.Cookie)
+ApiBase.post(f'/{version}/delete/product', onDeleteProduct, ['admin'], Auth.Cookie)
+ApiBase.post(f'/{version}/update/product/parentId', onUpdateProductPerantId, ['admin'], Auth.Cookie)
+ApiBase.post(f'/{version}/add/product', onAddProduct, ['admin'], Auth.Cookie)
